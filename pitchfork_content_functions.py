2372d7715bc749f12eb11f0c8e30fe3fd502644a
glmack <glmack@gmail.com>
2019-05-10 16:35:06 -0700

    removed unneeded notebooks

--- pitchfork_content_functions.py
+++
@@ -1,5 +0,0 @@
-def vectorize(corpus):
-    """learns vocab dictionary and returns feature names and term-document matrix"""
-    vectorizer = CountVectorizer(lowercase=False)
-    X = vectorizer.fit_transform(documents)
-    return vectorizer.get_feature_names(), X.toarray()
